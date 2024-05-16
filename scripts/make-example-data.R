fb5a22f3b0368a845f2baa6d1b4a674e7b58d41b
njlyon0 <nicholasjlyon@gmail.com>
2024-05-16 11:23:06 -0400

    Figured out how to randomize which plot (within site) has which response range

--- scripts/make-example-data.R
+++ scripts/make-example-data.R
@@ -42,22 +42,29 @@ for(k in 1:nrow(site_df)){
   # Define a site-specific modifier to change responses by
   site_mod <- site_df[k, ]$dist / 10
   
+  # Make a list of allowable ranges
+  rng_list <- list(15:22, 24:31, 1:6)
+  
+  # Randomize range order
+  rng_ord <- sample(x = 1:3, size = 3, replace = F)
+  
   # Generate annual data
   for(yr in 2022:2024){
     
     # Generate plot level data
-    for(plt in letters[1:3]){
+    for(plt in 1:3){
       
-      # Pick range of responses per site
-      if(plt == "a"){ rng <- 15:22 }
-      if(plt == "b"){ rng <- 24:31 }
-      if(plt == "c"){ rng <- 1:6 }
+      # Pick plot letter
+      plt_id <- letters[plt]
+      
+      # Grab range of responses at this plot
+      rng <- rng_list[[rng_ord[plt]]]
       
       # Modify the range by this site's modifier
       rng_mod <- ceiling(rng * site_mod)
       
       # Simulate response data 
-      resp <- sample(x = rng, size = reps, replace = T)
+      resp <- sample(x = rng_mod, size = reps, replace = T)
       
       # Generate a more involved data frame
       sim_df <- data.frame("site" = rep(x = site_df[k, ]$site, times = reps),
@@ -75,19 +82,30 @@ for(k in 1:nrow(site_df)){
   } # Close year loop
 } # Close site loop
 
-# Unlist and wrangle simulated data
+# Wrangle simulated data
 spider_df <- sim_list %>% 
-  purrr::list_rbind(x = .)
+  # Unlist to a dataframe
+  purrr::list_rbind(x = .) %>% 
+  # Put year in front of everything else
+  dplyr::relocate(year, .before = dplyr::everything())
 
 # Check structure
 dplyr::glimpse(spider_df)
 
 # Exploratory visuals
-ggplot(spider_df, aes(y = tarantula_count, x = reorder(site, -road_dist_km),
-                      fill = road_dist_km, color = plot)) +
+## Site means work as desired
+ggplot(spider_df, aes(y = tarantula_count, x = reorder(site, -road_dist_km), 
+                      fill = road_dist_km)) +
   geom_violin(alpha = 0.5) +
   theme_bw()
 
+## Plot-level random effect
+ggplot(spider_df, aes(y = tarantula_count, x = plot, fill = plot)) +
+  geom_violin(alpha = 0.5) +
+  facet_wrap(site ~ .) +
+  theme_bw()
+
 # Export locally
+# write.csv(x = spider_df, row.names = F, na = '', file = file.path("data", "tarantulas.csv"))
 
 # End ----
