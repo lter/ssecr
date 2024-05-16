10b20d83d13071a0eace8828205dd58997cadcc2
njlyon0 <nicholasjlyon@gmail.com>
2024-05-16 09:59:10 -0400

    Making a 'SSECR scripts' folder with a dedicated README to house a script for generating example data

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# SSECR Scripts
+
+It is periodically useful to have 'background scripts' for doing operations that support SSECR modules/topics but aren't valuable to include in the instructional materials in and of themselves. Such scripts live in this folder.
+
+For details on the purpose of a given script, open that script and check the internal documentation in the first few lines and consult the comments.
---
+++ scripts/make-example-data.R
@@ -0,0 +1,30 @@
+## ------------------------------------------------ ##
+                  # Make Example Data
+## ------------------------------------------------ ##
+# Script author(s): Nick J Lyon
+
+## Purpose:
+# Create example datasets that exactly meet our needs in a given module/topic
+
+## ------------------------------------ ##
+# Housekeeping ----
+## ------------------------------------ ##
+# Load needed libraries
+## install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls())
+
+## ------------------------------------ ##
+  # Stats - Mixed-Effect Case Study ----
+## ------------------------------------ ##
+# Simulate a tarantula dataset with nested structure
+
+
+
+
+
+
+
+# End ----
